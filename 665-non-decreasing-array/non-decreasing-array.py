e92f63eb260297f668f46d3d942206f2e477ae91
spaceybread <Spaceybread@gmail.com>
2024-08-21 13:22:07 +0530

    Time: 138 ms (95.08%) | Memory: 17.9 MB (9.85%) - LeetSync

---
+++ 665-non-decreasing-array/non-decreasing-array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def checkPossibility(self, nums: List[int]) -> bool:
+        c = 0
+        for i in range(1, len(nums)):
+            if nums[i] < nums[i - 1]:
+                if c or (i > 1 and i < len(nums) - 1 and nums[i - 2] > nums[i] and nums[i + 1] < nums[i - 1]):
+                    return False
+                c = 1
+        return True
