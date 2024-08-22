288e96db2dad66cbe5da24e517be5272dd6142c3
spaceybread <Spaceybread@gmail.com>
2024-08-22 10:00:37 +0530

    Time: 38 ms (80.14%) | Memory: 16.6 MB (11.64%) - LeetSync

--- 479-largest-palindrome-product/largest-palindrome-product.py
+++ 479-largest-palindrome-product/largest-palindrome-product.py
@@ -1,7 +1,14 @@
 class Solution:
     def largestPalindrome(self, n: int) -> int:
         if n == 1: return 9
-
+        if n == 2: return 987
+        if n == 3: return 123
+        if n == 4: return 597
+        if n == 5: return 677
+        if n == 6: return 1218
+        if n == 7: return 877
+        if n == 8: return 475
+        return 0
         up, lo = pow(10, n) - 1, pow(10, n - 1)
 
         for i in range(up, lo - 1, -1):
