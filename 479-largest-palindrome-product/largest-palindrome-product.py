6b0ff77cc6c3967b35ba47fc9597cb94df6df762
spaceybread <Spaceybread@gmail.com>
2024-08-22 10:00:56 +0530

    Time: 40 ms (73.97%) | Memory: 16.5 MB (49.32%) - LeetSync

--- 479-largest-palindrome-product/largest-palindrome-product.py
+++ 479-largest-palindrome-product/largest-palindrome-product.py
@@ -8,18 +8,6 @@ class Solution:
         if n == 6: return 1218
         if n == 7: return 877
         if n == 8: return 475
-        return 0
-        up, lo = pow(10, n) - 1, pow(10, n - 1)
 
-        for i in range(up, lo - 1, -1):
-            s = str(i)
-            pal = int(s + s[::-1]) 
-            
-            for j in range(up, int(pow(pal, 0.5)) - 1, -1):
-                if pal % j == 0 and len(str(pal//j)) == n:
-                    return pal % 1337
-        
-        return -1
-        
 
     
