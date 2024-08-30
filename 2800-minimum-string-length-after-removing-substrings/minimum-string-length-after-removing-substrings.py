993a5b63c9a94ecfac5f8f2c603219b136d74c19
spaceybread <Spaceybread@gmail.com>
2024-08-30 12:50:05 +0530

    Time: 50 ms (26.78%) | Memory: 16.5 MB (77.60%) - LeetSync

--- 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.py
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.py
@@ -2,12 +2,6 @@ class Solution:
     def minLength(self, s: str) -> int:
         c = 0
         while "AB" in s or "CD" in s:
-            s = list(s)
-
-            for i in range(1, len(s)):
-                if s[i - 1] == "A" and s[i] == "B" or s[i - 1] == "C" and s[i] == "D":
-                    s[i - 1], s[i] = "", ""
-                    c += 1
-            s = "".join(s)
-
+            s = s.replace("AB", "")
+            s = s.replace("CD", "")
         return len(s)
