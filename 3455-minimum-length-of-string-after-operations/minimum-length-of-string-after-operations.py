9ca27fbfe565889a8ba6efee8addf1f809ea0824
spaceybread <Spaceybread@gmail.com>
2025-01-14 03:17:09 +0530

    Time: 131 ms (68.85%) | Memory: 18.9 MB (33.77%) - LeetSync

--- 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.py
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.py
@@ -2,4 +2,4 @@ from collections import Counter
 
 class Solution:
     def minimumLength(self, s: str) -> int:
-        return sum(1 if c % 2 else 2 for c in Counter(s).values())
+        return sum(1 if c % 2 == 1 else 2 for c in Counter(s).values())
