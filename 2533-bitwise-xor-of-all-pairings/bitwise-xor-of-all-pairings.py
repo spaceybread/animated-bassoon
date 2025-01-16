b72bb02f272d1caaec1bb42225d1e92e5421d2a9
spaceybread <Spaceybread@gmail.com>
2025-01-16 06:22:42 +0530

    Time: 48 ms (11.56%) | Memory: 36.6 MB (48.56%) - LeetSync

--- 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.py
+++ 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.py
@@ -1,13 +1,5 @@
+from functools import reduce
+
 class Solution:
     def xorAllNums(self, nums1: List[int], nums2: List[int]) -> int:
-        n1, n2 = len(nums1) % 2, len(nums2) % 2
-        r = 0
-
-        for n in nums1:
-            for i in range(n2):
-                r ^= n
-        for n in nums2:
-            for i in range(n1):
-                r ^= n
-        
-        return r
+        return reduce(lambda r, x: r ^ x, [n for n in nums1 for _ in range(len(nums2) % 2)] + [n for n in nums2 for _ in range(len(nums1) % 2)], 0)
