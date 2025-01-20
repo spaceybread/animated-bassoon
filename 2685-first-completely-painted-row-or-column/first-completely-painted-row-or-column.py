37d2f99f745774443c71e48f0ca41c5a93cdccdd
spaceybread <Spaceybread@gmail.com>
2025-01-20 07:55:50 +0530

    Time: 155 ms (24.29%) | Memory: 39.9 MB (98.11%) - LeetSync

--- 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.py
+++ 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.py
@@ -1,7 +1,4 @@
 class Solution:
     def firstCompleteIndex(self, arr: List[int], mat: List[List[int]]) -> int:
         ids = sorted(range(len(mat[0])*len(mat)), key = lambda x: arr[x])
-        return min(
-            [max([ids[mat[i][j] - 1] for i in range(len(mat))]) for j in range(len(mat[0]))] +
-            [max([ids[mat[i][j] - 1] for j in range(len(mat[0]))]) for i in range(len(mat))]
-        )
+        return min([max([ids[mat[i][j] - 1] for i in range(len(mat))]) for j in range(len(mat[0]))] + [max([ids[mat[i][j] - 1] for j in range(len(mat[0]))]) for i in range(len(mat))])
