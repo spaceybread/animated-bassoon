10fc22a3ef532fdba8844e69fc1603b54c0e0604
spaceybread <Spaceybread@gmail.com>
2025-02-13 13:03:55 -0600

    Time: 265 ms (27.31%) | Memory: 35.6 MB (38.24%) - LeetSync

--- 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.py
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.py
@@ -5,9 +5,8 @@ class Solution:
         heapq.heapify(a)
         out = 0
 
-        while len(a) > 1:
+        while a[0] < k:
             x = heapq.heappop(a)
-            if x >= k: return out
             y = heapq.heappop(a)
             out += 1
             heapq.heappush(a, min(x, y) * 2 + max(x, y)) 
