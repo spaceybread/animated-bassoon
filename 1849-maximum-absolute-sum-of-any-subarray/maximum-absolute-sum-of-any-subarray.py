27f3fd9c122a90fdaf47ddf88b5f4344374c656a
spaceybread <Spaceybread@gmail.com>
2025-02-25 23:29:32 -0600

    Time: 27 ms (87.81%) | Memory: 29.5 MB (7.32%) - LeetSync

--- 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.py
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.py
@@ -1,5 +1,3 @@
 class Solution:
     def maxAbsoluteSum(self, nums: List[int]) -> int:
-        ps = [0]
-        for a in nums: ps.append(a + ps[-1])
-        return max(ps) - min(ps)
+        return max(list(accumulate(nums, initial=0))) - min(list(accumulate(nums, initial=0)))
