d2250b38e50db92cc72c93e6136a3a0e3401909d
spaceybread <Spaceybread@gmail.com>
2025-02-25 23:29:48 -0600

    Time: 20 ms (91.46%) | Memory: 29.3 MB (10.98%) - LeetSync

--- 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.py
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.py
@@ -1,3 +1,4 @@
 class Solution:
     def maxAbsoluteSum(self, nums: List[int]) -> int:
+        # I like single line bullshit
         return max(list(accumulate(nums, initial=0))) - min(list(accumulate(nums, initial=0)))
