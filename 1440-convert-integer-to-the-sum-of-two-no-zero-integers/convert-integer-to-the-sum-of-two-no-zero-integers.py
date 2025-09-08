5c3b0900cba5d77691f4eccdf03b2b2c7da139d8
spaceybread <Spaceybread@gmail.com>
2025-09-08 12:22:55 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (12.69%) - LeetSync

--- 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.py
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.py
@@ -1,9 +1,11 @@
+import random
+
 class Solution:
     def getNoZeroIntegers(self, n: int) -> List[int]:
-        a, b = 1, n - 1
+        a, b = 0, 0
 
-        while '0' in str(b) or '0' in str(a): 
-            a += 1
-            b -= 1
-
+        while '0' in str(a) or '0' in str(b):
+            a = random.randint(1, n - 1)
+            b = n - a
+        
         return [a, b]
