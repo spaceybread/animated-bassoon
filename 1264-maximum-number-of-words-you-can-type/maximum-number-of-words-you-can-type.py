572cac203ef6be477a3d5c0e827b31ec3bd07449
spaceybread <Spaceybread@gmail.com>
2025-09-14 20:07:26 -0500

    Time: 3 ms (36.59%) | Memory: 18.1 MB (16.85%) - LeetSync

--- 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.py
+++ 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.py
@@ -1,10 +1,3 @@
 class Solution:
     def canBeTypedWords(self, text: str, brokenLetters: str) -> int:
-        bad = set(list(brokenLetters))
-        words = text.split(' ')
-        out = 0
-        for word in words:
-            if set(list(word)).intersection(bad) == set(): 
-                out += 1
-        return out
-
+        return sum([1 if set(list(word)).intersection(set(list(brokenLetters))) == set() else 0 for word in text.split(' ')])
