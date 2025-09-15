ea6ed2be1214a494749245e780c3cb59d9c7b555
spaceybread <Spaceybread@gmail.com>
2025-09-14 20:05:47 -0500

    Time: 3 ms (36.59%) | Memory: 17.6 MB (98.00%) - LeetSync

--- 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.py
+++ 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.py
@@ -1,6 +1,10 @@
 class Solution:
-    def canBeTypedWords(self, text: str, brk: str) -> int:
-        brkSet = set(brk)
-        c = sum(not set(w) & brkSet for w in text.split())
+    def canBeTypedWords(self, text: str, brokenLetters: str) -> int:
+        bad = set(list(brokenLetters))
+        words = text.split(' ')
+        out = 0
+        for word in words:
+            if set(list(word)).intersection(bad) == set(): 
+                out += 1
+        return out
 
-        return c
