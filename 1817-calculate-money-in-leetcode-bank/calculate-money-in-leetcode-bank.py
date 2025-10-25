5dca46d4be6c9893b468bc918421427a77747aae
spaceybread <Spaceybread@gmail.com>
2025-10-25 17:56:40 -0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (89.27%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.py
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.py
@@ -1,14 +1,7 @@
 class Solution:
-    def ab(self, a, b):
-        return (b * (b + 1) - a * (a + 1)) // 2
+    def ab(self, a, b): return (b * (b + 1) - a * (a + 1)) // 2
 
     def totalMoney(self, n: int) -> int:
-        k = n // 7
-        r = n % 7
+        return sum([self.ab(i, i + 7) for i in range(n // 7)]) + self.ab((n // 7), (n // 7) + (n % 7))
 
-        s = 0
-        for i in range(k): s += self.ab(i, i + 7)
-        
-        s += self.ab(k, k + r)
-
-        return s
+         
