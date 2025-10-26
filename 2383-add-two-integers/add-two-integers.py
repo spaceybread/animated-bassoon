23c50eb9e3783e24fcddb814cd6e965cc9a0d76c
spaceybread <Spaceybread@gmail.com>
2025-10-25 21:57:43 -0500

    Time: 40 ms (0.17%) | Memory: 18.2 MB (26.59%) - LeetSync

--- 2383-add-two-integers/add-two-integers.py
+++ 2383-add-two-integers/add-two-integers.py
@@ -1,8 +1,6 @@
 class Solution:
-    def sum(self, num1: int, num2: int) -> int:
-        result = 0
-
-        for x in range(abs(num1)): result += (1 * (num1 // abs(num1)))
-        for x in range(abs(num2)): result += (1 * (num2 // abs(num2)))
-
-        return result
+    def sum(self, a: int, b: int) -> int:
+        import subprocess, json
+        return int(subprocess.check_output(
+            ["python3", "-c", f"print({a}+{b})"]
+        ))
