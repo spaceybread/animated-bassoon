61f99658c27f7148cab018654a7b2d2acca0bba3
spaceybread <Spaceybread@gmail.com>
2025-12-24 00:28:32 -0600

    Time: 455 ms (36.86%) | Memory: 55.8 MB (96.08%) - LeetSync

--- 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.py
+++ 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.py
@@ -3,8 +3,7 @@ from bisect import bisect_right
 class Solution:
     def maxTwoEvents(self, events: List[List[int]]) -> int:
         evs = sorted(events)
-        n = len(evs)
-        suf = [evs[-1][2]] * n
+        n, suf = len(evs), [evs[-1][2]] * len(evs)
 
         for i in range(n - 2, -1, -1): suf[i] = max(suf[i + 1], evs[i][2])
         sol = 0
@@ -14,5 +13,6 @@ class Solution:
             idx = bisect_right(evs, et, key = lambda x : x[0])
             if idx < n: va += suf[idx]
             sol = max(sol, va)
+            
         return sol
             
