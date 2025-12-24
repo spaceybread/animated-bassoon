669f836aeb9a7f400c94f31eabc6c662d2719de1
spaceybread <Spaceybread@gmail.com>
2025-12-24 00:31:31 -0600

    Time: 427 ms (37.64%) | Memory: 55.9 MB (95.69%) - LeetSync

--- 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.py
+++ 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.py
@@ -4,15 +4,15 @@ class Solution:
     def maxTwoEvents(self, events: List[List[int]]) -> int:
         evs = sorted(events)
         n, suf = len(evs), [evs[-1][2]] * len(evs)
+        sol = 0
 
         for i in range(n - 2, -1, -1): suf[i] = max(suf[i + 1], evs[i][2])
-        sol = 0
 
         for k in evs:
             et, va = k[1], k[2]
             idx = bisect_right(evs, et, key = lambda x : x[0])
             if idx < n: va += suf[idx]
             sol = max(sol, va)
-            
+
         return sol
             
