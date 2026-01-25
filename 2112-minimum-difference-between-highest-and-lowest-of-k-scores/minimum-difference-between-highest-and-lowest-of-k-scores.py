ef054401f3409772a35eebaa2fe0e1391591977c
spaceybread <Spaceybread@gmail.com>
2026-01-25 12:28:04 -0600

    Time: 6 ms (81.52%) | Memory: 19.4 MB (15.85%) - LeetSync

--- 2112-minimum-difference-between-highest-and-lowest-of-k-scores/minimum-difference-between-highest-and-lowest-of-k-scores.py
+++ 2112-minimum-difference-between-highest-and-lowest-of-k-scores/minimum-difference-between-highest-and-lowest-of-k-scores.py
@@ -3,8 +3,5 @@ class Solution:
         ns, n = sorted(nums), len(nums)
         if k == 1: return 0
         m = max(nums) + 1
-        print(ns)
-        for i in range(n - k + 1): 
-            print(ns[i + k - 1], ns[i])
-            m = min(m, ns[i + k - 1] - ns[i])
+        for i in range(n - k + 1): m = min(m, ns[i + k - 1] - ns[i])
         return m
