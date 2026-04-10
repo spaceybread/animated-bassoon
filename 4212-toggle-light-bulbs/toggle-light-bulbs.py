b5ae497ca94d0fd3fbb4ba9005508d4ec3fc0073
spaceybread <Spaceybread@gmail.com>
2026-04-09 20:05:48 -0500

    Time: 59 ms (5.17%) | Memory: 19.4 MB (69.64%) - LeetSync

--- 4212-toggle-light-bulbs/toggle-light-bulbs.py
+++ 4212-toggle-light-bulbs/toggle-light-bulbs.py
@@ -1,3 +1,3 @@
 class Solution:
     def toggleLightBulbs(self, bulbs: list[int]) -> list[int]:
-        return sorted(list(set([x for x in bulbs if Counter(bulbs)[x] % 2 == 1])))
+        return sorted([x for x in set(bulbs) if Counter(bulbs)[x] % 2 == 1])
