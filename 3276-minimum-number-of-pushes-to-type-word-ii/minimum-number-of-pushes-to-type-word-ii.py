478f195c51159f5c649dcf8c41398b2842a467cb
spaceybread <Spaceybread@gmail.com>
2026-07-31 13:36:02 +0530

    Time: 115 ms (45.82%) | Memory: 20.8 MB (9.03%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
@@ -1,14 +1,16 @@
 class Solution:
     def minimumPushes(self, word: str) -> int:
-        lc = [0] * 26
-        for c in word:
-            lc[ord(c) - ord('a')] += 1
-        lc = sorted(lc)[::-1]
+        from collections import Counter as ct
+        p1, p2, p3, p4 = 8, 8, 8, 2
+        w = list(word)
+        cw = ct(w)
+        ls = sorted(list(set(w)), key = lambda x : cw[x])[::-1]
 
         c = 0
-        for i, k in enumerate(lc):
-            if k == 0:
-                break
-            c += (i // 8 + 1) * k
-        
+        for i in range(len(ls)):
+            x = ls[i]
+            if i < 8: c += cw[x]
+            elif 8 <= i < 16: c += 2 * cw[x]
+            elif 16 <= i < 24: c += 3 * cw[x]
+            else: c += 4 * cw[x]
         return c
