3f765ae9c20d627fd2619bff5f7f5fef3b3f904c
spaceybread <Spaceybread@gmail.com>
2026-08-04 18:44:13 +0530

    Time: 3 ms (69.59%) | Memory: 19.3 MB (18.85%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.py
+++ 4107-find-missing-elements/find-missing-elements.py
@@ -1,5 +1,5 @@
 class Solution:
     def findMissingElements(self, nums: List[int]) -> List[int]:
-        return list(filter(lambda x : x > 0, [x if x not in nums else -1 for x in range(min(nums) + 1, max(nums))]))
-        
+        return [x for x in range(min(nums) + 1, max(nums)) if x not in nums]
+        # return list(filter(lambda x : x > 0, [x if x not in nums else -1 for x in range(min(nums) + 1, max(nums))]))
         # return sorted(list(set(range(min(nums), max(nums))) - set(nums)))
